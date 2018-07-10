af878b12814583e14e40074f6d15be7383f0dda1
SeverZmith <eric.g.smith.24@gmail.com>
2018-07-09 23:05:02 -0700

    Initial commit

---
+++ Source/NetworkTestGame/NetworkTestGame.cpp
@@ -0,0 +1,7 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#include "NetworkTestGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, NetworkTestGame, "NetworkTestGame" );
+ 
